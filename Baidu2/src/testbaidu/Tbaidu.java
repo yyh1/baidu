c1e305fc695860796741415d1addffebcc2c66e4
yyh1 <1372902989@qq.com>
2019-03-14 14:12:20 +0800

    baidu 自动化

---
+++ Baidu2/src/testbaidu/Tbaidu.java
@@ -0,0 +1,17 @@
+package testbaidu;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class Tbaidu{
+	@Test
+	public void openchrome() throws InterruptedException {
+		   //¹È¸èä¯ÀÀÆ÷
+		  
+	System.setProperty("webdriver.chrome.driver",".//tools//chromedriver.exe");
+	   WebDriver driver=new ChromeDriver();
+	  driver.get("http://www.baidu.com");
+}
+}
---
+++ Baidu2/testbaidu/NewTest.java
@@ -0,0 +1,9 @@
+package testbaidu;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+}
